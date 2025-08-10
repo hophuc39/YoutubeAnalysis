a5becd87c70e3a699a69dc9de6d18b2e7f4ee7aa
Hồ Phúc <122793785+hophuc39@users.noreply.github.com>
2025-08-10 12:39:09 +0700

    chore: bootstrap codebase

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000")
+      .then(res => res.text())
+      .then(data => setMessage(data));
+  }, []);
+
+  return <h1>{message}</h1>;
+}
+
+export default App
