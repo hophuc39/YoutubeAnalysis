27574f9bd216b551fc0910a7d8e09d3786da071b
Hồ Phúc <122793785+hophuc39@users.noreply.github.com>
2025-08-10 16:03:15 +0700

    feat(frontend): setup a form for receiving url, add shadcnui dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,66 @@
-import { useEffect, useState } from 'react'
-
+import { useState } from 'react';
 import './App.css'
+import { useForm } from 'react-hook-form';
+import { FormProvider } from 'react-hook-form';
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from './components/ui/form';
+import { Button } from './components/ui/button';
+import { Input } from './components/ui/input';
+import z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
 
 function App() {
-  const [message, setMessage] = useState("");
+  const formSchema = z.object({
+    youtubeUrl: z.url("Invalid YouTube URL"),
+  })
 
-  useEffect(() => {
-    fetch("http://localhost:5000")
-      .then(res => res.text())
-      .then(data => setMessage(data));
-  }, []);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      youtubeUrl: '',
+    }
+  })
 
-  return <h1>{message}</h1>;
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    setLoading(true);
+    try {
+      // delay for ui testing
+      await new Promise((resolve) => setTimeout(resolve, 1500));  
+      console.log("Submitted URL:", data.youtubeUrl);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <div className="App">
+      <FormProvider {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="youtubeUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Youtube URL</FormLabel>
+                <FormControl>
+                  <Input placeholder="https://youtube/..." {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is the URL of the YouTube video you want to analyze.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button variant="outline" type="submit" disabled={loading}>
+            {loading ? 'Analyzing...' : 'Analyze'}
+          </Button>
+        </form>
+      </FormProvider>
+    </div>
+  );
 }
 
 export default App
